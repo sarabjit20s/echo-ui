7641af8cae43f45556da62d3398689f4733c56b2
Sarabjit Singh <heysarabjit@gmail.com>
2024-12-19 20:26:09 +0530

    feat(docs): add metadata and favicon

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -2,9 +2,30 @@ import './global.css';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { RootProvider } from 'fumadocs-ui/provider';
-import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
-export default function Layout({ children }: { children: ReactNode }) {
+export const metadata: Metadata = {
+  title: 'Echo UI',
+  description:
+    'A collection of pre-built, high-quality React Native components designed to accelerate your mobile app development.',
+  keywords: [
+    'React Native',
+    'React Native UI Library',
+    'React Native UI Components Library',
+    'Expo',
+    'Unistyles',
+    'Unistyles UI Library',
+    'Unistyles UI Components Library',
+  ],
+  creator: 'sarabjit',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang="en"
