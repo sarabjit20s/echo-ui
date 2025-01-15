7fa03d632729e9bafe969fb4c95d759f506c717a
Sarabjit Singh <heysarabjit@gmail.com>
2025-01-16 00:56:27 +0530

    chore(www): add eslint-disable-next-line check

--- apps/www/app/api/registry/items/route.ts
+++ apps/www/app/api/registry/items/route.ts
@@ -28,6 +28,7 @@ export async function GET(request: NextRequest) {
       );
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     if (itemType && !registryItemTypes.includes(itemType as any)) {
       return Response.json(
         {
