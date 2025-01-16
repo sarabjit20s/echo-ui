3a9739d9e3501a051e2c0f3fc8902cce4734fa11
Sarabjit Singh <heysarabjit@gmail.com>
2025-01-17 01:09:03 +0530

    chore(registry): refactor

--- packages/registry/src/index.ts
+++ packages/registry/src/index.ts
@@ -1,2 +1,4 @@
-export { registry } from './registry';
-export { registryWithCode } from './registry-with-code';
+export * from './schema';
+export * from './registry';
+export * from './registry-with-code';
+export * from './utils/get-registry-items-with-code';
